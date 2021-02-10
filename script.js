aaeb443a7071e3d5e7c6e6d68a1adf97e31ae034
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-10 17:00:14 +0000

    get rid of semicolons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-alert('Hello World'); ;  
-document.write('Hello World'); ;  
-document.write('<br>'); ;  
-var str = new String('Hello World with color'); ;   
-document.write(str.fontcolor('blue')); ;  
+alert('Hello World')
+document.write('Hello World')
+document.write('<br>')
+var str = new String('Hello World with color')
+document.write(str.fontcolor('blue'))
