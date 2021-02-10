ec3c438d9ae60c86ea9bc49225c8a64e4c41af9f
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-10 16:48:57 +0000

    added second semicolon

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-alert('Hello World'); 
-document.write('Hello World'); 
+alert('Hello World');; 
+document.write('Hello World');; 
+document.write("<br>");
+var str = new String('Hello World with color'); 
+document.write(str.fontcolor("blue")); 
