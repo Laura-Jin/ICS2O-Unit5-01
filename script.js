2dfaa86fada62202af56ce3bbd8d8769e9fc9dcd
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-10 16:07:29 +0000

    Changes to single quotations and added a second semicolon

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-alert("Hello World");
-document.write("Hello World");
-//next lines are for extra
-document.write("<br>"); //makes a new line
-var str = new String ("Hello World with colour"); //makes a new variable called str that when used, writes out demo. String is a series of characters
-document.write(str.fontcolor("blue")); //turns the variable str which writes Demo, into a blue color
+alert('Hello World');;
+document.write('Hello World');;
